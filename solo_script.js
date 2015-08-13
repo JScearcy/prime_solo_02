4134193a48ceac278293fc4b15473c79250f9168
JScearcy <jacobscearcy@gmail.com>
2015-08-13 11:21:58 -0500

    change to objects

--- solo_script.js
+++ solo_script.js
@@ -1,4 +1,5 @@
-//Object constructor
+
+//object constructor for beginning object
 var Employee = function(name, empNum, sal, rat){
 	this.name = name;
 	this.empNum = empNum;
@@ -21,8 +22,11 @@ newArray[1] = ("Bonus percentage: " + bonus * 100 + "%");
 newArray[2] = ("Adjusted salary: $" + (Math.round(baseSalary * (1.0 + bonus))).toLocaleString());
 newArray[3] = ("Bonus dollars: $" + (Math.round(baseSalary * bonus)).toLocaleString());
 console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-return newArray;
+//change array into a new object and return that
+var newEmp = new empBonus(newArray[0], newArray[1], newArray[2], newArray[3]);
+return newEmp;
 }
+//prototype for STI function
 Employee.prototype.getBaseSTI = function(reviewScore){
   var basePercent;
   switch(reviewScore){
@@ -38,6 +42,7 @@ Employee.prototype.getBaseSTI = function(reviewScore){
   }
   return basePercent;
 }
+//prototype for get Year function
 Employee.prototype.getYearAdjustment = function(employeeNumber){
   var yearAdjustment = 0;
   if(employeeNumber.length == 4){
@@ -45,6 +50,7 @@ Employee.prototype.getYearAdjustment = function(employeeNumber){
   }
   return yearAdjustment;
 }
+//prototype for get Income function
 Employee.prototype.getIncomeAdjustment = function(salary){
   var incomeAdjustment = 0;
   salary = parseInt(salary);
@@ -54,7 +60,23 @@ Employee.prototype.getIncomeAdjustment = function(salary){
   return incomeAdjustment;
 }
 
-
+//Object constructor for final object
+var empBonus = function(name, bonusPer, adjSal, bonus){
+	this.name = name;
+	this.bonusPer = bonusPer;
+	this.adjSal = adjSal;
+	this.bonus = bonus;
+}
+//function to convert object into array
+empBonus.prototype.convertToArray = function(){
+	var array = [];
+	array[0] = this.name;
+	array[1] = this.bonusPer;
+	array[2] = this.adjSal;
+	array[3] = this.bonus;
+	return array;
+}
+//create objects
 var Atticus = new Employee("Atticus", "2405", "47000", 3);
 var Jem = new Employee("Jem", "62347", "63500", 4);
 var Boo = new Employee("Boo", "11435", "54000", 3);
@@ -62,8 +84,9 @@ var Scout = new Employee("Scout", "6243", "74750", 5);
 //place objects into array
 var empArray = [Atticus, Jem, Boo, Scout];
 var bonusArray = [];
+var objArray = [];
 //Create variables used to write to the DOM
-var newEl, newText, position, spaced;
+var newEl, newText, position;
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
 
@@ -71,8 +94,10 @@ position = document.getElementById('content');
 //Note that the information is not 'clean'
 for(var i = 0; i < empArray.length; i++){
 	bonusArray[i] = empArray[i].calculateSTI();
+	objArray[i] = bonusArray[i].convertToArray();
  	newEl = document.createElement('li');
-	newText = document.createTextNode(bonusArray[i].join(', '));
+	newText = document.createTextNode(objArray[i].join(', '));
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
+console.log(bonusArray);
